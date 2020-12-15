5affb507ff93ad79136f644cae30aa7bd994ca03
themanmaran <tyler.maran@gmail.com>
2020-12-14 22:42:34 -0500

    added howto page to welcome email

--- src/templates/users/welcome/text.js
+++ src/templates/users/welcome/text.js
@@ -4,6 +4,8 @@ const text = `
   Open source needs you now more than ever before. And we want you to get paid for it too.
   
   So take a look around, find something to work on and fund what you believe in.
+
+  Questions? Head over to our How-to page (https://rysolv.com/how-to) or reach out to support@rysolv.com.
   
   The Rysolv Team
   https://rysolv.com
